fb916c3375232aceccecaef16431d8e68205e557
Joohyung <54580802+jooh9992@users.noreply.github.com>
2022-12-02 12:16:43 +0900

    Feat: 토큰 발급 API 구현

--- Node/apiclient/routes/index.js
+++ Node/apiclient/routes/index.js
@@ -1,8 +1,49 @@
 const express = require('express');
 const axios = require('axios');
-const { Router } = require('express');
+
+//매번 동일한 요청을 위한 URL을 상수로 설정
+const URL = 'http://localhost:8000/v1';
+//ajax 요펑을 할 때 누가 요청했는지 확인해주기 위해서
+//origin header 추가
+axios.defaults.headers.origin = 'http://localhost:4000';
+
+//토큰 발급 코드
+const request = async(req, api) =>{
+    try{
+        if(!req.session.jwt){
+            const tokenResult = await axios.post(`${URL}/token`,{
+                clientSecret:process.env.CLIENT_SECRET
+            });
+            req.session.jwt = tokenResult.data.token;
+        }
+        //토큰 내용 확인
+        const result = await axios.get(`${URL}/${api}`,{
+            headers:{authorization:req.session.jwt}
+        })
+        return result;
+    }catch(error){
+        //토큰 유효 기간 만료
+        if(error.response.status === 419){
+            //기존 토큰 삭제
+            delete req.session.jwt;
+            //다시 토큰을 생성해달라고 요청
+            return request(req, api);
+        }
+        return error.response;
+    }
+}
 
 const router = express.Router();
+
+router.get('/mypost', async(req, res, next)=>{
+    try{
+        const result = await request(req, '/posts/my');
+        res.json(result.data);
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
+})
 
 router.get('/test', async(req, res, next)=>{
     try{
