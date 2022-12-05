349ade6dba764656e6a774a8e58f7f04f4d1f55c
Joohyung <54580802+jooh9992@users.noreply.github.com>
2022-12-05 09:11:20 +0900

    Feat: react 프로젝트 생성

---
+++ react/react_component/src/App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import MyComponent from './MyComponent';
+class App extends Component{
+  render(){
+    return (<div><MyComponent /><MyComponent /></div>)
+  }
+}
+
+export default App;
