f9fb5c9f0648a500c12d78198acf7043b1bfd612
Joohyung <54580802+jooh9992@users.noreply.github.com>
2022-12-06 09:17:34 +0900

    Fix: ref 사용한 버튼 오류 수정

--- react/react_component/src/App.js
+++ react/react_component/src/App.js
@@ -3,18 +3,22 @@ import StateComponent from './StateComponent';
 import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <>
-      <MyComponent name={3}>태그 안의 내용</MyComponent>
-      <StateComponent/>
-      <EventPractice/>
-      <ValidationSample/>
-      <ScrollBox ref={(ref)=>this.scrollbox = ref}/>
-      <button onClick={(e)=>{this.scrollbox.scrollToBottom()}}>맨 아래로</button>
-    </>
-  );
+class App extends Component {
+  render(){
+    return (
+      <>
+        <MyComponent name={3}>태그 안의 내용</MyComponent>
+        <StateComponent/>
+        <EventPractice/>
+        <ValidationSample/>
+        <ScrollBox ref={(ref)=>this.scrollbox = ref}/>
+        <button onClick={(e)=>{this.scrollbox.scrollToBottom()}}>맨 아래로</button>
+      </>
+    );
+  }
+  
 }
 
 export default App;
